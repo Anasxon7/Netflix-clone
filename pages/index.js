c47a1c952fb31af2c8f0ce291fc5b5e5bab86bf7
Madaminov <muhammadanasxon6694@gmail.com>
2022-09-20 00:44:09 +0500

    new changes

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ const Index = () => (
   <div css={xw`grid justify-center items-center h-screen space-y-20`}>
     <div css={xw`space-y-20`}>
       <ButtonReact>@emotion/react</ButtonReact>
-      <ButtonStyled>@emotion/styled</ButtonStyled>
     </div>
   </div>
 )
